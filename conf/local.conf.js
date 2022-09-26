b729b55dcbb827305bc7279a3760a3d50a37bf61
Neha Agarwal <neha@Nehas-MacBook-Pro.local>
2022-09-26 12:42:30 +0530

    Added source capability and corrected buildName and sessionName

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -10,8 +10,9 @@ exports.config = {
       'bstack:options': {
         'os': 'Windows',
         'osVersion': '7',
-        'sessionName': 'local_test',
-        'buildName': 'cucumber-js-browserstack',
+        'sessionName': 'Bstack local cucumber-js',
+        'buildName': 'browserstack-build-1',
+        'source': 'cucumber-js:sample-selenium-4-v1.0',
         'projectName': 'Test App',
         'debug': true,
         'local': true
