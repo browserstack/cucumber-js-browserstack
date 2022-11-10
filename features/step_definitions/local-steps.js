4750a39e86b22acfc4cb50f9b50c63486bec8277
07souravkunda <souravkunda00@gmail.com>
2022-11-10 10:44:33 +0530

    update: local test

--- features/step_definitions/local-steps.js
+++ features/step_definitions/local-steps.js
@@ -3,11 +3,11 @@
 var assert = require('assert');
 const { When, Then } = require("@cucumber/cucumber");
 
-When(/^I open health check$/, async function () {
-  await this.driver.get('http://bs-local.com:45691/check');
+When(/^I open dashboard$/, async function () {
+  await this.driver.get('http://bs-local.com:45454/');
 });
 
 Then(/^I should see "([^"]*)"$/, async function (sourceMatch) {
-  let source = await this.driver.getPageSource();
-  assert.equal(source.indexOf(sourceMatch) > -1, true, 'Expected source to contain ' + sourceMatch);
+  let title = await this.driver.getTitle();
+  assert.equal(title.indexOf(sourceMatch) > -1, true, 'Expected source to contain ' + sourceMatch);
 });
